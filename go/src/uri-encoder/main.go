dd61f23ccc62bbb58f78ca1f4885018a95311ec6
dave-read <davre@microsoft.com>
2017-09-08 23:05:21 -0700

    add json serialization

--- go/src/uri-encoder/main.go
+++ go/src/uri-encoder/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"net/url"
 	"os"
@@ -9,29 +11,69 @@ import (
 const (
 	service = ".postgres.database.azure.com"
 	scheme  = "postgres"
-	port    = "5432"
+	port    = 5432
 )
 
 var (
-	server   = getEnvVarOrExit("server")
-	database = getEnvVarOrExit("database")
-	username = getEnvVarOrExit("user")
-	password = getEnvVarOrExit("password")
+	username = `a"b`    //getEnvVarOrExit("user")
+	password = `c"d`    //getEnvVarOrExit("password")
+	database = "db"     //getEnvVarOrExit("database")
+	server   = "server" //getEnvVarOrExit("server")
 )
+
+/*
+
+ {
+  "hostname": "db-hostname",
+  "port": 5432,
+  "database": "postgres",
+  "username": "username",
+  "password": "password",
+  "uri": "postgres://username:password@db-hostname:5432/dbname",
+  "uuid": "4ac6cb37-f486-4d71-a339-eb46bce4e399",
+  "allocated_storage": 10,
+  "maintenance_window": "fri:03:00-fri:03:30"
+ }
+
+*/
+
+// ServiceDefinition is serialized to JSON
+type ServiceDefinition struct {
+	Hostname string `json:"hostname,omitempty"`
+	Port     int    `json:"port,omitempty"`
+	Database string `json:"database,omitempty"`
+	Username string `json:"username,omitempty"`
+	Password string `json:"password,omitempty"`
+	URI      string `json:"uri,omitempty"`
+}
 
 // postgres://test$1@dr-test:{your_password}@dr-test.postgres.database.azure.com:5432/test$1
 func main() {
 
 	uri := url.URL{}
 	uri.Scheme = scheme
-	uri.Host = server + service + ":" + port
+	uri.Host = server + service + ":" + string(port)
 	uri.User = url.UserPassword(username+"@"+server, password)
 	uri.Path = database
 	// removed query string as there's no single portable representation and its not in the bind sample
 	//q := uri.Query()
 	//q.Set("ssl", "true")
 	//uri.RawQuery = q.Encode()
+	serviceDefintion := ServiceDefinition{
+		Hostname: server,
+		Port:     port,
+		Database: database,
+		Password: password,
+		Username: uri.User.Username(),
+		URI:      uri.String(),
+	}
+
 	fmt.Println(uri.String())
+
+	fmt.Printf("struct:%+v", serviceDefintion)
+
+	fmt.Println(marshalToJSON(serviceDefintion))
+
 }
 
 // getEnvVarOrExit returns the value of specified environment variable or terminates if it's not defined.
@@ -43,4 +85,24 @@ func getEnvVarOrExit(varName string) string {
 	}
 
 	return value
+}
+
+func encodeToJSON(v interface{}) string {
+
+	buf := new(bytes.Buffer)
+	enc := json.NewEncoder(buf)
+	enc.SetEscapeHTML(true)
+	err := enc.Encode(v)
+	if err != nil {
+		return "ERROR:" + err.Error()
+	}
+	return buf.String()
+}
+
+func marshalToJSON(v interface{}) string {
+	j, err := json.MarshalIndent(v, "", "  ")
+	if err != nil {
+		return "ERROR:" + err.Error()
+	}
+	return string(j)
 }
